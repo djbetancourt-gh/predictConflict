eca6d6493070770885c1a5c726790cd961349c62
djbetancourt-gh <djbetancourt@uninorte.edu.co>
2020-01-17 12:00:15 +0100

    initial commit
    the initial files

---
+++ R/model.R
@@ -0,0 +1,41 @@
+#' @title Class: apk model
+#' @description To create an apk object, use \link[aPack]{apk}. See also this function for mode details.
+#' @slot call Object of class \code{"language"}. User call reminder.
+#' @rdname apk-class
+#' @import methods
+#' @export
+setClass("apk", representation(call = "language"), validity = function(object) {T})
+
+#' @title Create an Object of class \code{"apk"}
+#' @description Creator function for objects of class \code{"apk"}.
+#' @param foo Not used yet.
+#' @param ... Not used yet.
+#' @export
+apk <- function(foo, ...) {
+    new("apk")
+}
+
+#' @name predict
+#' @rdname predict-methods
+#' @importFrom stats predict
+#' @exportMethod predict
+#' @param object An object to predict from.
+#' @param ... Further arguments for methods.
+if (!isGeneric("predict")) {
+  setGeneric(name = "predict", def = function(object, ...) standardGeneric("predict"))
+}
+
+
+predict.apk <- function(object, bar,  ...) {
+  print("I'm an apk prediction!")
+}
+
+#' @title Prediction Method for the apk Class
+#' @name predict
+#' @rdname predict-methods
+#' @aliases predict,apk-method
+#' @param bar Not used yet.
+#' @examples
+#' myApk <- apk()
+#' predict(myApk)
+setMethod("predict", "apk", predict.apk)
